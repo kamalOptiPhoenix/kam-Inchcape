ed90e08f77e79361822ba8136d123d5c3f446993
krupagothadiya <krupa.gothadiya@optiphoenix.com>
2026-06-26 15:46:16 +0530

    global code updated

--- 100-Inchcape/v9gmgapcwz-Peugeot AU PROD/kam_globalcode.js
+++ 100-Inchcape/v9gmgapcwz-Peugeot AU PROD/kam_globalcode.js
@@ -1,61 +1,96 @@
 console.log('*** Global Code ***');
-const pageURL = window.location.href;
+
 const globalGoals = {
-    'KAM - Make an Enquiry Conversion': 420578,
-    'KAM - Book a Test Drive Conversion': 420579,
+	'KAM - Make an Enquiry Conversion': 420578,
+	'KAM - Book a Test Drive Conversion': 420579,
 };
-const optimizelyGoals = [
-    'make_an_enquiry_conversion',
-    'book_a_test_drive_conversion'
-];
+
 (function globalTrackingAndTargeting() {
-    const modules = {
-        tracking: {
-            push_API_only: (nameOfEvent) => {
-                window.optimizely = window.optimizely || [];
-                window.optimizely.push({
-                    type: 'event',
-                    eventName: nameOfEvent,
-                });
-            },
-            formSubmitHandler: () => {
-                jQuery(document).on(
-                    'submit',
-                    'form#buildbuy_testdrive_enquiry_form, form#buildbuy_email_dealer_form',
-                    (e) => {
-                        const formBody = e.target;
-                        const wrapper = formBody.parentElement;
-                        Kameleoon.API.Core.runWhenConditionTrue(
-                            () => wrapper.querySelector('div.buildFormSuccessWrapper'),
-                            () => {
-                                if (wrapper.id === 'buildbuy_email_dealer') {
-                                    console.log('%c *** make_an_enquiry_conversion goal triggered ***', 'background-color: white');
-                                    modules.tracking.push_API_only('make_an_enquiry_conversion');
-                                    if (Kameleoon?.API?.Goals?.processConversion) {
-                                        Kameleoon.API.Goals.processConversion(globalGoals['KAM - Make an Enquiry Conversion']);
-                                    }
-                                } else if (wrapper.id === 'buildbuy_testdrive_enquiry') {
-                                    console.log('%c *** book_a_test_drive_conversion goal triggered ***', 'background-color: white');
-                                    modules.tracking.push_API_only('book_a_test_drive_conversion');
-                                    if (Kameleoon?.API?.Goals?.processConversion) {
-                                        Kameleoon.API.Goals.processConversion(globalGoals['KAM - Book a Test Drive Conversion']);
-                                    }
-                                }
-                            }
-                        );
-                    }
-                );
-            }
-        },
-        targeting: {
-            pages: {
-               configuratorSummary: () => window.location.pathname.includes('/build-and-buy/build/summary/'),
-            }
-        }
-    }
-     // Scripts running on configuratorSummary page
-     Kameleoon.API.Core.runWhenConditionTrue(modules.targeting.pages.configuratorSummary, () => {
-        console.log('*** Global Tracking and Targeting [ configuratorSummary page ] ***');
-        modules.tracking.formSubmitHandler();
-     });
+	const modules = {
+		tracking: {
+			push_API_only: (eventName) => {
+				window.optimizely = window.optimizely || [];
+				window.optimizely.push({
+					type: 'event',
+					eventName,
+				});
+			},
+
+			formSubmitHandler: () => {
+				// Prevent attaching the listener multiple times
+				if (window.__kameleoonGlobalSubmitHandlerAttached) {
+					return;
+				}
+
+				window.__kameleoonGlobalSubmitHandlerAttached = true;
+
+				document.addEventListener('submit', (e) => {
+					const form = e.target;
+
+					if (
+						!form.matches(
+							'form#buildbuy_testdrive_enquiry_form, form#buildbuy_email_dealer_form'
+						)
+					) {
+						return;
+					}
+
+					console.log('*** Form Submitted ***');
+
+					const wrapper = form.parentElement;
+
+					Kameleoon.API.Core.runWhenConditionTrue(
+						() => !!wrapper?.querySelector('.buildFormSuccessWrapper'),
+						() => {
+							if (wrapper.id === 'buildbuy_email_dealer') {
+								console.log(
+									'%c*** make_an_enquiry_conversion goal triggered ***',
+									'background: yellow; color: black;'
+								);
+
+								modules.tracking.push_API_only('make_an_enquiry_conversion');
+
+								Kameleoon.API.Goals.processConversion(
+									globalGoals['KAM - Make an Enquiry Conversion']
+								);
+							} else if (wrapper.id === 'buildbuy_testdrive_enquiry') {
+								console.log(
+									'%c*** book_a_test_drive_conversion goal triggered ***',
+									'background: lightgreen; color: black;'
+								);
+
+								modules.tracking.push_API_only('book_a_test_drive_conversion');
+
+								Kameleoon.API.Goals.processConversion(
+									globalGoals['KAM - Book a Test Drive Conversion']
+								);
+							}
+						}
+					);
+				});
+			},
+		},
+
+		targeting: {
+			pages: {
+				configuratorSummary: () =>
+					window.location.pathname.includes('/build-and-buy/build/summary/'),
+			},
+
+			bodyReady: () => document.body !== null,
+		},
+	};
+
+	Kameleoon.API.Core.runWhenConditionTrue(
+		() =>
+			modules.targeting.pages.configuratorSummary() &&
+			modules.targeting.bodyReady(),
+		() => {
+			console.log(
+				'*** Global Tracking and Targeting [ configuratorSummary page ] ***'
+			);
+
+			modules.tracking.formSubmitHandler();
+		}
+	);
 })();
